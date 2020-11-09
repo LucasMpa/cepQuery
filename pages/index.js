8f09e8e572633113567bccb4e0a38265fc9dba13
Lucas M <lucasmpa16@gmail.com>
2020-11-09 20:37:32 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <div>
+      <h3>Home</h3>
+      <Contador />
+    </div>
+  );
+}
+
+function Contador() {
+  const [count, setCount] = useState(1);
+
+  function AdicionarContador() {
+    setCount(count + 1);
+  }
+  function ReduzirContador() {
+    if (count === 0) {
+      alert("Não é possível reduzir para menos que 0");
+    } else 
+    setCount(count - 1);
+  }
+  return (
+    <div>
+      <h3>{count}</h3>
+      <br />
+      <button onClick={AdicionarContador}>Adicionar</button>
+      <button onClick={ReduzirContador}>Reduzir</button>
+    </div>
+  );
+}
+
+export default Home;
